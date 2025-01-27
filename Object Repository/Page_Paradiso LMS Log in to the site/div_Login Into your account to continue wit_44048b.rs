e35e9437c4470b728067f7b7e822bc3a86e8c0ba
hp <hp@DESKTOP-20CTJHV.Optilink>
2025-01-27 12:14:19 +0530

    Login Test

---
+++ Object Repository/Page_Paradiso LMS Log in to the site/div_Login Into your account to continue wit_44048b.rs
@@ -0,0 +1,430 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Login Into your account to continue wit_44048b</name>
+   <tag></tag>
+   <elementGuidId>fb13d5cd-ca30-4163-b03e-5f218664f5c1</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#login_center</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='login_center']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>#login_center</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>75a69c29-bcb4-4bc9-b1af-72c7e5b61ddf</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>login_center</value>
+      <webElementGuid>cefab1a8-d5b2-4b91-8091-3c1897f92ad9</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>row h-100</value>
+      <webElementGuid>a4fee025-87b4-4694-8af5-3c209b859d8a</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+            
+                
+                    
+                        
+                            
+                                    
+                            
+                            
+                            
+                                Login Into your account to continue with your learning experience.  
+                            
+
+                                
+                                    Skip to create new account
+                                
+
+                            
+                                
+                                    
+                                        
+                                            
+                                            
+                                            document.getElementById('anchor').value = location.hash;
+                                            Username
+                                            
+                                                
+                                                
+                                                
+                                                        Username
+                                                
+                                                
+                                                
+                                            
+                                            Password
+                                            Forgot your password?
+                                            
+                                            
+                                                
+                                                Password
+                                                
+                                                
+                                                
+                                                
+                                            
+                                            
+                                            
+                                             
+    
+        
+        
+        English ‎(en)‎     
+    
+    
+                English ‎(en)‎
+                Español - Internacional ‎(es)‎
+                Japanese ‎(ja_kids)‎
+                हिंदी ‎(hi)‎
+                日本語 ‎(ja)‎
+    
+
+                                            
+                                                
+                                                    
+                                                    Keep me logged in
+                                                
+                                            
+                                            
+                                            Log in
+                                            
+                                        
+                                    
+                                    
+                                
+                                
+                                    
+                                        Or you can join with
+                                        
+                                                
+                                                    
+                                                            
+                                                    
+                                                
+                                                
+                                                    
+                                                            
+                                                    
+                                                
+                                                
+                                                    
+                                                            
+                                                    
+                                                
+                                        
+                                    
+                                    
+                                                       
+                            
+                                    
+                                        Don’t have an  account? Create An Account   
+                                    
+                        
+                    
+                
+            
+        
+                
+                Powered by ParadisoLMS
+                </value>
+      <webElementGuid>38b6ace0-3828-4222-af15-0a0ac2743b62</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;login_center&quot;)</value>
+      <webElementGuid>ea9f2d62-e61d-4fa6-af7a-574d63da93c3</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//div[@id='login_center']</value>
+      <webElementGuid>8b1d7d6a-fb01-4d7e-a446-9b2dad48e014</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='yui_3_17_2_1_1737960148018_20']/div</value>
+      <webElementGuid>d12cbf89-fbe2-4d6b-b416-09260d40e2b0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Skip to main content'])[1]/following::div[4]</value>
+      <webElementGuid>ca71990a-0f1c-4a50-a8fe-b295e7ead09e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div/div</value>
+      <webElementGuid>eb835e11-f3e2-477c-8203-027e2e82c4ba</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[@id = 'login_center' and (text() = concat(&quot;
+            
+                
+                    
+                        
+                            
+                                    
+                            
+                            
+                            
+                                Login Into your account to continue with your learning experience.  
+                            
+
+                                
+                                    Skip to create new account
+                                
+
+                            
+                                
+                                    
+                                        
+                                            
+                                            
+                                            document.getElementById(&quot; , &quot;'&quot; , &quot;anchor&quot; , &quot;'&quot; , &quot;).value = location.hash;
+                                            Username
+                                            
+                                                
+                                                
+                                                
+                                                        Username
+                                                
+                                                
+                                                
+                                            
+                                            Password
+                                            Forgot your password?
+                                            
+                                            
+                                                
+                                                Password
+                                                
+                                                
+                                                
+                                                
+                                            
+                                            
+                                            
+                                             
+    
+        
+        
+        English ‎(en)‎     
+    
+    
+                English ‎(en)‎
+                Español - Internacional ‎(es)‎
+                Japanese ‎(ja_kids)‎
+                हिंदी ‎(hi)‎
+                日本語 ‎(ja)‎
+    
+
+                                            
+                                                
+                                                    
+                                                    Keep me logged in
+                                                
+                                            
+                                            
+                                            Log in
+                                            
+                                        
+                                    
+                                    
+                                
+                                
+                                    
+                                        Or you can join with
+                                        
+                                                
+                                                    
+                                                            
+                                                    
+                                                
+                                                
+                                                    
+                                                            
+                                                    
+                                                
+                                                
+                                                    
+                                                            
+                                                    
+                                                
+                                        
+                                    
+                                    
+                                                       
+                            
+                                    
+                                        Don’t have an  account? Create An Account   
+                                    
+                        
+                    
+                
+            
+        
+                
+                Powered by ParadisoLMS
+                &quot;) or . = concat(&quot;
+            
+                
+                    
+                        
+                            
+                                    
+                            
+                            
+                            
+                                Login Into your account to continue with your learning experience.  
+                            
+
+                                
+                                    Skip to create new account
+                                
+
+                            
+                                
+                                    
+                                        
+                                            
+                                            
+                                            document.getElementById(&quot; , &quot;'&quot; , &quot;anchor&quot; , &quot;'&quot; , &quot;).value = location.hash;
+                                            Username
+                                            
+                                                
+                                                
+                                                
+                                                        Username
+                                                
+                                                
+                                                
+                                            
+                                            Password
+                                            Forgot your password?
+                                            
+                                            
+                                                
+                                                Password
+                                                
+                                                
+                                                
+                                                
+                                            
+                                            
+                                            
+                                             
+    
+        
+        
+        English ‎(en)‎     
+    
+    
+                English ‎(en)‎
+                Español - Internacional ‎(es)‎
+                Japanese ‎(ja_kids)‎
+                हिंदी ‎(hi)‎
+                日本語 ‎(ja)‎
+    
+
+                                            
+                                                
+                                                    
+                                                    Keep me logged in
+                                                
+                                            
+                                            
+                                            Log in
+                                            
+                                        
+                                    
+                                    
+                                
+                                
+                                    
+                                        Or you can join with
+                                        
+                                                
+                                                    
+                                                            
+                                                    
+                                                
+                                                
+                                                    
+                                                            
+                                                    
+                                                
+                                                
+                                                    
+                                                            
+                                                    
+                                                
+                                        
+                                    
+                                    
+                                                       
+                            
+                                    
+                                        Don’t have an  account? Create An Account   
+                                    
+                        
+                    
+                
+            
+        
+                
+                Powered by ParadisoLMS
+                &quot;))]</value>
+      <webElementGuid>caa94899-d700-478d-9096-69254c2649ac</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
