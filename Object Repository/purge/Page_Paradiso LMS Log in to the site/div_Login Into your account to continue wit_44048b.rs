6961d103b7cd9bbe921e74967ca30e98c0f03f4f
hp <hp@DESKTOP-20CTJHV.Optilink>
2025-04-04 16:33:04 +0530

    j

---
+++ Object Repository/purge/Page_Paradiso LMS Log in to the site/div_Login Into your account to continue wit_44048b.rs
@@ -0,0 +1,439 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Login Into your account to continue wit_44048b</name>
+   <tag></tag>
+   <elementGuidId>0a37521b-0ccc-492a-9227-d7525b71c2d5</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='login_center']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#login_center</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>d5067ef8-68bf-46c3-8f43-e107c2643b20</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>login_center</value>
+      <webElementGuid>4b10e9dc-6f67-4fbb-83e6-165703d441dd</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>row h-100</value>
+      <webElementGuid>48b5c1a6-1fa7-4a1b-b90a-02c9afb1d1d6</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+            
+                
+                    
+                        
+                            
+                                    
+                            
+                            
+                            
+                                Login Into your account to continue with your learning experience.  
+                            
+
+                                
+                                    Skip to create new account
+                                
+
+                            
+                                
+                                    
+                                        
+                                            
+                                            
+                                            document.getElementById('anchor').value = location.hash;
+                                            Username
+                                            
+                                                
+                                                
+                                                
+                                                        Username
+                                                
+                                                
+                                                
+                                            
+                                            Password
+                                            Forgot your password?
+                                            
+                                            
+                                                
+                                                Password
+                                                
+                                                
+                                                
+                                                
+                                            
+                                            
+                                            
+                                             
+    
+        
+        
+        English ‎(en)‎     
+    
+    
+                English ‎(en)‎
+                Español - Internacional ‎(es)‎
+                Français ‎(fr)‎
+                العربية ‎(ar)‎
+                हिंदी ‎(hi)‎
+    
+
+                                            
+                                                
+                                                    
+                                                    Keep me logged in
+                                                
+                                            
+                                            
+                                            Log in
+                                            
+                                        
+                                    
+                                    
+                                
+                                
+                                    
+                                        Or you can join with
+                                        
+                                                
+                                                    
+                                                            
+                                                    
+                                                
+                                                
+                                                    
+                                                            
+                                                    
+                                                
+                                                
+                                                    
+                                                            
+                                                    
+                                                
+                                                
+                                                    
+                                                            
+                                                    
+                                                
+                                        
+                                    
+                                    
+                                                       
+                            
+                                    
+                                        Don’t have an  account? Create An Account   
+                                    
+                        
+                    
+                
+            
+        
+                
+                Powered by ParadisoLMS
+                </value>
+      <webElementGuid>bee38efa-f7ca-4311-8261-5328aab41dd8</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;login_center&quot;)</value>
+      <webElementGuid>fd7d272c-d392-4dca-95ad-f386fe430279</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//div[@id='login_center']</value>
+      <webElementGuid>bc81b971-b393-4e29-a557-ec520594bdf5</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//section[@id='region-main']/div/div</value>
+      <webElementGuid>900447e3-c8c1-4c7d-bb4e-f21c00e8a196</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Skip to main content'])[1]/following::div[4]</value>
+      <webElementGuid>145f9611-a884-41ce-8153-cb2a5d9662cd</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div/div</value>
+      <webElementGuid>4e38a454-8053-469b-8f33-6eb5cb8abc84</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[@id = 'login_center' and (text() = concat(&quot;
+            
+                
+                    
+                        
+                            
+                                    
+                            
+                            
+                            
+                                Login Into your account to continue with your learning experience.  
+                            
+
+                                
+                                    Skip to create new account
+                                
+
+                            
+                                
+                                    
+                                        
+                                            
+                                            
+                                            document.getElementById(&quot; , &quot;'&quot; , &quot;anchor&quot; , &quot;'&quot; , &quot;).value = location.hash;
+                                            Username
+                                            
+                                                
+                                                
+                                                
+                                                        Username
+                                                
+                                                
+                                                
+                                            
+                                            Password
+                                            Forgot your password?
+                                            
+                                            
+                                                
+                                                Password
+                                                
+                                                
+                                                
+                                                
+                                            
+                                            
+                                            
+                                             
+    
+        
+        
+        English ‎(en)‎     
+    
+    
+                English ‎(en)‎
+                Español - Internacional ‎(es)‎
+                Français ‎(fr)‎
+                العربية ‎(ar)‎
+                हिंदी ‎(hi)‎
+    
+
+                                            
+                                                
+                                                    
+                                                    Keep me logged in
+                                                
+                                            
+                                            
+                                            Log in
+                                            
+                                        
+                                    
+                                    
+                                
+                                
+                                    
+                                        Or you can join with
+                                        
+                                                
+                                                    
+                                                            
+                                                    
+                                                
+                                                
+                                                    
+                                                            
+                                                    
+                                                
+                                                
+                                                    
+                                                            
+                                                    
+                                                
+                                                
+                                                    
+                                                            
+                                                    
+                                                
+                                        
+                                    
+                                    
+                                                       
+                            
+                                    
+                                        Don’t have an  account? Create An Account   
+                                    
+                        
+                    
+                
+            
+        
+                
+                Powered by ParadisoLMS
+                &quot;) or . = concat(&quot;
+            
+                
+                    
+                        
+                            
+                                    
+                            
+                            
+                            
+                                Login Into your account to continue with your learning experience.  
+                            
+
+                                
+                                    Skip to create new account
+                                
+
+                            
+                                
+                                    
+                                        
+                                            
+                                            
+                                            document.getElementById(&quot; , &quot;'&quot; , &quot;anchor&quot; , &quot;'&quot; , &quot;).value = location.hash;
+                                            Username
+                                            
+                                                
+                                                
+                                                
+                                                        Username
+                                                
+                                                
+                                                
+                                            
+                                            Password
+                                            Forgot your password?
+                                            
+                                            
+                                                
+                                                Password
+                                                
+                                                
+                                                
+                                                
+                                            
+                                            
+                                            
+                                             
+    
+        
+        
+        English ‎(en)‎     
+    
+    
+                English ‎(en)‎
+                Español - Internacional ‎(es)‎
+                Français ‎(fr)‎
+                العربية ‎(ar)‎
+                हिंदी ‎(hi)‎
+    
+
+                                            
+                                                
+                                                    
+                                                    Keep me logged in
+                                                
+                                            
+                                            
+                                            Log in
+                                            
+                                        
+                                    
+                                    
+                                
+                                
+                                    
+                                        Or you can join with
+                                        
+                                                
+                                                    
+                                                            
+                                                    
+                                                
+                                                
+                                                    
+                                                            
+                                                    
+                                                
+                                                
+                                                    
+                                                            
+                                                    
+                                                
+                                                
+                                                    
+                                                            
+                                                    
+                                                
+                                        
+                                    
+                                    
+                                                       
+                            
+                                    
+                                        Don’t have an  account? Create An Account   
+                                    
+                        
+                    
+                
+            
+        
+                
+                Powered by ParadisoLMS
+                &quot;))]</value>
+      <webElementGuid>2440701e-1153-4966-99aa-56da8ea3a591</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
